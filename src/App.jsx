3c7dee6e53db05433bf137331badcc8b6290b34b
RakeshV0498 <rockr4674@gmail.com>
2024-04-25 07:30:14 +0530

    Created Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<h1>Home</h1>}></Route>
+          <Route path="/users" exact element={<h1>Users</h1>}></Route>
+          <Route path="/users/:userId" element={<h1>User</h1>}></Route>
+          <Route path="/create" element={<h1>Create User</h1>}></Route>
+          <Route path="*" element={<h1>Page Not found</h1>}></Route>
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
