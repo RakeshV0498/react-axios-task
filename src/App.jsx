4e43bad782d91a91fe9b3051ef9534ee5a3fdf0f
RakeshV0498 <rockr4674@gmail.com>
2024-04-25 19:12:06 +0530

    Included UserForm component Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" exact element={<Home />}></Route>
           <Route path="/users" exact element={<h1>Users</h1>}></Route>
           <Route path="/users/:userId" element={<h1>User</h1>}></Route>
-          <Route path="/create" element={<h1>Create User</h1>}></Route>
+          <Route path="/create" element={<UserForm />}></Route>
           <Route path="*" element={<h1>Page Not found</h1>}></Route>
         </Routes>
       </>
