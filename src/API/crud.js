76560745b9f5ff8ab98960117884f1f9a460edcf
RakeshV0498 <rockr4674@gmail.com>
2024-04-25 07:51:55 +0530

    Created read method

--- src/API/crud.js
+++ src/API/crud.js
@@ -1,1 +1,22 @@
+import axios from "axios";
+
 const url = "https://jsonplaceholder.typicode.com/users/";
+
+export const readAllData = async () => {
+  try {
+    const response = await axios.get(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error reading the data:", error);
+    throw error;
+  }
+};
+
+// readAllData()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
