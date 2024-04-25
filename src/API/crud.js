c7be9d6f6025d05607737f0b89813c2bbe46a255
RakeshV0498 <rockr4674@gmail.com>
2024-04-25 07:59:18 +0530

    Updated read functionality

--- src/API/crud.js
+++ src/API/crud.js
@@ -5,8 +5,7 @@ const url = "https://jsonplaceholder.typicode.com/users/";
 export const readAllData = async () => {
   try {
     const response = await axios.get(url);
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
     console.error("Error reading the data:", error);
     throw error;
