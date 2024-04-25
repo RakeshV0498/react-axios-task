c5fef90da702645a5f90252b30ab3767adf78785
RakeshV0498 <rockr4674@gmail.com>
2024-04-25 07:58:17 +0530

    Implemented Home Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <>
         <Header />
         <Routes>
-          <Route path="/" exact element={<h1>Home</h1>}></Route>
+          <Route path="/" exact element={<Home />}></Route>
           <Route path="/users" exact element={<h1>Users</h1>}></Route>
           <Route path="/users/:userId" element={<h1>User</h1>}></Route>
           <Route path="/create" element={<h1>Create User</h1>}></Route>
